20deb5e11c3cbfe4c0062f161da68de549ef1c45
k2 <22292600+k-128@users.noreply.github.com>
2021-07-01 10:46:56 +0200

    upd. -u output

--- pip_upgrade_all.py
+++ pip_upgrade_all.py
@@ -7,11 +7,11 @@ from pprint import PrettyPrinter
 from math import floor
 
 
-PACKAGES = [dist.project_name for dist in pkg_resources.working_set]
-CMD_PIP_VERS = [sys.executable, "-m", "pip", "-V"]
+PACKAGES        = [dist.project_name for dist in pkg_resources.working_set]
+CMD_PIP_VERS    = [sys.executable, "-m", "pip", "-V"]
 CMD_PIP_UPGRADE = [sys.executable, "-m", "pip", "install", "--upgrade"]
-HELP_CMD = "'-h', '--help'"
-HELP_MSG = """Available commands:
+HELP_CMD        = "'-h', '--help'"
+HELP_MSG        = """Available commands:
 -h, --help
     Print this message.
 -l, --list
@@ -21,7 +21,7 @@ HELP_MSG = """Available commands:
 -u, --upgrade
     Upgrade all installed packages.
 -uD, --upgrade-debug
-    Upgrade all packages with full pip output."""
+    Upgrade all packages while printing pip output."""
 
 
 def _with_progress_bar(func, symbol="■"):
@@ -31,18 +31,16 @@ def _with_progress_bar(func, symbol="■"):
         while True:
             try:
                 progress = next(gen)
-
             except StopIteration as e:
                 sys.stdout.write("\n")
                 return e.value
-
             else:
-                space_to_pct = [3, 2, 1][len(str(round(progress))) - 1]
-                msg = f"[%s]{' ' * space_to_pct}{progress}%%"
-                bar_width = max_width - len(msg) + 2
-                filled = int(round(bar_width / 100 * progress))
-                space_left = bar_width - filled
-                bar = f"{symbol * filled}{' ' * space_left}"
+                space_to_pct  = [3, 2, 1][len(str(round(progress))) - 1]
+                msg           = f"[%s]{' ' * space_to_pct}{progress}%%"
+                bar_width     = max_width - len(msg) + 2
+                filled        = int(round(bar_width / 100 * progress))
+                space_left    = bar_width - filled
+                bar           = f"{symbol * filled}{' ' * space_left}"
                 sys.stdout.write(f"{msg}\r" % bar)
                 sys.stdout.flush()
 
@@ -59,12 +57,14 @@ def upgrade_all_packages():
     i = 0
     with Popen(cmd, stdout=SPROC_PIPE, universal_newlines=True) as p:
         for line in p.stdout:
-            if line[:31] == "Requirement already up-to-date:":
+            if ((line[:31] == "Requirement already up-to-date:"
+                    or line[:30] == "Requirement already satisfied:")
+                and i < len(PACKAGES)
+            ):
                 i += 1
                 yield floor(i * progress_per_package)
-
             else:
-                print(f"{line}")
+                pass  #print(f"{line}")
 
     yield 100
 
@@ -74,28 +74,23 @@ def main():
     pp = PrettyPrinter(indent=4, width=max_width, compact=True)
 
     if len(sys.argv) == 1:
-        print(f"Arg required, for help: {HELP_CMD}")
-
+        print(f"Arg required, help: {HELP_CMD}")
     elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
         print(f"{HELP_MSG}")
-
     elif sys.argv[1] == "-l" or sys.argv[1] == "--list":
         pp.pprint(PACKAGES)
-
     elif sys.argv[1] == "-v" or sys.argv[1] == "--versions":
         sp_run(CMD_PIP_VERS)
         print(f"Python: {sys.version}")
-
     elif sys.argv[1] == "-u" or sys.argv[1] == "--upgrade":
         print("Upgrading all packages...")
         upgrade_all_packages()
-
+        print("Done")
     elif sys.argv[1] == "-uD" or sys.argv[1] == "--upgrade-debug":
         print("Upgrading all packages with full pip output...")
         sp_run(CMD_PIP_UPGRADE + PACKAGES)
-
     else:
-        print(f"Invalid arg, for help: {HELP_CMD}")
+        print(f"Invalid arg, help: {HELP_CMD}")
 
 
 if __name__ == "__main__":
